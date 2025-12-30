7375356d26f89b66f8065794950fc6a760a816cc
kumanamu <searchstock1@gmail.com>
2025-12-30 23:18:29 +0900

    배포수정중

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// vite.config.ts
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': '/src'  // ← path 없이!
     }
   },
   server: {
